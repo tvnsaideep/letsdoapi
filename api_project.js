2d71f061a4ec5e5459e2b50980ba5e3d73f41fda
tvnsaideep <tvnsaideep@gmail.com>
2020-08-19 23:45:30 +0530

    pushing api code

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Todo Application",
+  "version": "1.0.0",
+  "description": "Documentation for Todo Application",
+  "title": "APIs for Todo Application",
+  "url": "http://localhost:4200",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-08-19T16:48:29.332Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
